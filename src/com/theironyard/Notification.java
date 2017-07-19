314d654d598761ff35812a380f07108d034ef3bf
Joe <joe-sawyer32@users.noreply.github.com>
2017-07-19 12:24:00 -0500

    Initial commit

---
+++ src/com/theironyard/Notification.java
@@ -0,0 +1,16 @@
+package com.theironyard;
+
+/**
+ * Created by Joe on 7/19/17.
+ */
+class Notification {
+
+}
+
+class EmailNotification extends Notification {
+
+}
+
+class TextNotification extends Notification {
+
+}
