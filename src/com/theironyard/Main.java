9880c360b07711cd2bb8f015c6cb1f4118395d0d
Joe <joe-sawyer32@users.noreply.github.com>
2017-07-19 15:37:56 -0500

    Protected fields and methods done

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -3,6 +3,17 @@ package com.theironyard;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        EmailNotification myEmailNotice = new EmailNotification("Company Policy", "This is a reminder of " +
+                "some company policy", "@all_employees", "Hotmail");
+        myEmailNotice.printText();
+        myEmailNotice.transport();
+        myEmailNotice.setStatus("Sent");
+        myEmailNotice.showStatus();
+
+        TextNotification myTextNotice = new TextNotification("Flash Flood Warning", "Davidson County is " +
+                "under a flash flood warning until 5:00 PM", "@all615", "Verizon");
+
+        myTextNotice.showStatus();
+        myTextNotice.transport();
     }
 }
--- src/com/theironyard/Notification.java
+++ src/com/theironyard/Notification.java
@@ -10,12 +10,14 @@ class Notification {
     private LocalDateTime createdAt;
     private String subject;
     private String body;
+    protected String status;
 
     // CONSTRUCTORS
     public Notification(String subject, String body) {
         this.subject = subject;
         this.body = body;
         this.createdAt = LocalDateTime.now();
+        this.status = "Waiting";
     }
 
     // METHODS
@@ -35,6 +37,14 @@ class Notification {
     // custom
     public void transport() {
         throw new NoTransportException();
+    }
+
+    public void showStatus() {
+        System.out.println("Status: " + this.status);
+    }
+
+    protected void printText() {
+        System.out.println("\nFrom Notification\n");
     }
 }
 
@@ -60,6 +70,11 @@ class EmailNotification extends Notification {
         return smtpProvider;
     }
 
+    // setters
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
     // custom
     @Override
     public void transport() {
@@ -69,14 +84,45 @@ class EmailNotification extends Notification {
         System.out.println("Notification Recipient: " + getRecipient());
         System.out.println("Notification SMTP Provider: " + getSmtpProvider());
     }
+
+    @Override
+    public void printText() {
+        System.out.println("\nFrom EmailNotification\n");
+    }
 }
 
 class TextNotification extends Notification {
     // FIELDS
+    private String recipient;
+    private String smsProvider;
 
     // CONSTRUCTORS
+    public TextNotification(String subject, String body, String recipient, String smsProvider) {
+        super(subject, body);
+        this.recipient = recipient;
+        this.smsProvider = smsProvider;
+        printText();
+    }
 
     // METHODS
+    // getters
+    public String getRecipient() {
+        return recipient;
+    }
+
+    public String getSmsProvider() {
+        return smsProvider;
+    }
+
+    // custom
+    @Override
+    public void transport() {
+        System.out.println("Notification Subject: " + getSubject());
+        System.out.println("Notification Body: " + getBody());
+        System.out.println("Notification Creation: " + getCreatedAt());
+        System.out.println("Notification Recipient: " + getRecipient());
+        System.out.println("Notification SMS Provider: " + getSmsProvider());
+    }
 }
 
 class NoTransportException extends RuntimeException {
